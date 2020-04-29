64806ca850776674ff5c30379f62f9c2d9c456a1
MSMSalim <salim.belim@moneysupermarket.com>
2020-04-29 22:52:25 +0100

    route with click event back to products page from details page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'pm-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-  public pageTitle : string = 'Product Detail';
-  public product : IProduct;
-  
-  constructor() { }
+  public pageTitle: string = 'Product Detail';
+  public product: IProduct;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    console.log(this.route.snapshot.paramMap.get('id'));
+
+    this.pageTitle += `: ${id}`;
+
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png',
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
